a59f5a9ee68fb92dbe3a9092275a699695739cea
Daniel Lanao <daniel.lanao.s@gmail.com>
2025-09-22 15:44:44 -0300

    Test welcome 2

---
+++ resources/views/wlecome2.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master_auth')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Welcome 2</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
